32ced82c6ee24bb44adbf26a4090fbfa69d27676
闫昱丰 <yan.yf@aliyun.com>
2017-01-12 15:15:04 +0800

    Add files via upload

---
+++ sort-integers.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+'''
+@Copyright:LintCode
+@Author:   yfyan
+@Problem:  http://www.lintcode.com/problem/sort-integers
+@Language: Python
+@Datetime: 17-01-12 06:57
+'''
+
+class Solution:
+    # @param {int[]} A an integer array
+    # @return nothing
+    def sortIntegers(self, A):
+        # Write your code here
+        for i in xrange(len(A) - 1):
+            for j in xrange(len(A) - i - 1):
+                if A[j] >= A[j+1]:
+                    A[j], A[j+1] = A[j+1], A[j]
