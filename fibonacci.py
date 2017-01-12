e70bdb4582d276e9a5764a24ee87b3201355a109
闫昱丰 <yan.yf@aliyun.com>
2017-01-12 16:37:40 +0800

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+'''
+@Copyright:LintCode
+@Author:   yfyan
+@Problem:  http://www.lintcode.com/problem/fibonacci
+@Language: Python
+@Datetime: 17-01-12 07:44
+'''
+
+class Solution:
+    # @param n: an integer
+    # @return an integer f(n)
+    def fibonacci(self, n):
+        # write your code here
+        if n == 1:
+            return 0
+        elif n == 2:
+            return 1
+        else:
+            a = 0
+            b = 1
+            r = 0
+            for i in range(2, n):
+                r = a + b
+                a, b = b, r
+            return r
---
+++ remove-linked-list-elements.py
@@ -0,0 +1,35 @@
+# coding:utf-8
+'''
+@Copyright:LintCode
+@Author:   yfyan
+@Problem:  http://www.lintcode.com/problem/remove-linked-list-elements
+@Language: Python
+@Datetime: 17-01-12 08:20
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    # @param head, a ListNode
+    # @param val, an integer
+    # @return a ListNode
+    def removeElements(self, head, val):
+        # Write your code here
+        if not head:
+            return head
+        else:
+            dummy = ListNode(0)
+            dummy.next = head
+            pre = dummy
+            while head:
+                if head.val == val:
+                    pre.next = head.next
+                    head = pre
+                pre = head
+                head = head.next
+            return dummy.next
